05391e5d5584bacdec245b28d82b163a2884b3b6
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2025-04-02 10:44:26 +0200

    Création du repository Personne

---
+++ src/main/java/fr/afpa/harmonia/EcoleMusique_API/repositories/PersonneRepository.java
@@ -0,0 +1,9 @@
+package fr.afpa.harmonia.EcoleMusique_API.repositories;
+
+import fr.afpa.harmonia.EcoleMusique_API.models.Personne;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonneRepository extends JpaRepository<Personne, Integer> {
+}
