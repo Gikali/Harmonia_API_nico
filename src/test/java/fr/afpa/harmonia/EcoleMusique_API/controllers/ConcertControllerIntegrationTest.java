c097e6c7663b0c399ff1912dba4e521f262feb82
AlexandreSama <alexandrehainy12@gmail.com>
2025-04-02 11:39:55 +0200

    Ajout du test classique et du test d'intégration

---
+++ src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/ConcertControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package fr.afpa.harmonia.EcoleMusique_API.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ConcertControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void findById() throws Exception {
+        mockMvc.perform(get("/concerts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].nomConcert").value(is("Tomorrowland")));
+    }
+}
---
+++ src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/ConcertControllerTest.java
@@ -0,0 +1,28 @@
+package fr.afpa.harmonia.EcoleMusique_API.controllers;
+
+import fr.afpa.harmonia.EcoleMusique_API.repositories.ConcertRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ConcertController.class)
+public class ConcertControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ConcertRepository concertRepository;
+
+    @Test
+    public void findAll() throws Exception {
+        mockMvc.perform(get("/concerts")).andExpect(status().isOk());
+    }
+}
