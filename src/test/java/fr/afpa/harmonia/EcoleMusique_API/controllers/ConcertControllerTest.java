634b01234eb10f9177ef5983d8c21de46c492612
AlexandreSama <alexandrehainy12@gmail.com>
2025-04-02 12:43:14 +0200

    Ajout d'un test d'ajout de concert

--- src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/ConcertControllerTest.java
+++ src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/ConcertControllerTest.java
@@ -1,13 +1,18 @@
 package fr.afpa.harmonia.EcoleMusique_API.controllers;
 
+import fr.afpa.harmonia.EcoleMusique_API.models.Concert;
 import fr.afpa.harmonia.EcoleMusique_API.repositories.ConcertRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ConcertController.class)
@@ -22,5 +27,29 @@ public class ConcertControllerTest {
     @Test
     public void findAll() throws Exception {
         mockMvc.perform(get("/concerts")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void addConcert() throws Exception {
+        // Création d'un concert sans identifiant (tel qu'il est envoyé par le client)
+        Concert concertToSave = new Concert();
+        concertToSave.setNomConcert("NewConcert");
+
+        // Simulation du comportement du repository :
+        // lorsqu'on sauvegarde, on renvoie un concert avec identifiant généré
+        Concert savedConcert = new Concert();
+        savedConcert.setIdentifiantConcert(5);
+        savedConcert.setNomConcert("NewConcert");
+
+        when(concertRepository.save(any(Concert.class))).thenReturn(savedConcert);
+
+        // JSON à envoyer dans la requête POST
+        String jsonConcert = "{\"nomConcert\":\"NewConcert\"}";
+
+        // Effectue la requête POST et vérifie le résultat
+        mockMvc.perform(post("/concert/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonConcert))
+                .andExpect(status().isOk());
     }
 }
