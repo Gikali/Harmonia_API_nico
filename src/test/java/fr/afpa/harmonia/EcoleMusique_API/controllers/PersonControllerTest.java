e0ad24f9b20f7f74411837d2a684cac7dc1d5f8b
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2025-04-03 14:40:08 +0200

    Tentative de mise en place de tests

--- src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/PersonControllerTest.java
+++ src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/PersonControllerTest.java
@@ -3,13 +3,15 @@ package fr.afpa.harmonia.EcoleMusique_API.controllers;
 import fr.afpa.harmonia.EcoleMusique_API.models.Personne;
 import fr.afpa.harmonia.EcoleMusique_API.repositories.PersonneRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,22 +24,85 @@ public class PersonControllerTest {
     @MockitoBean
     private PersonneRepository personneRepository;
 
+
+    /**
+     * Test de l'affichage de toutes les personnes
+     *
+     * @throws Exception
+     */
     @Test
     public void getPersonnesTest() throws Exception {
         mockMvc.perform(get("/personnes")).andExpect(status().isOk());
     }
 
+    /**
+     * Test de l'ajout d'une personne
+     *
+     * @throws Exception
+     */
     @Test
-    public void getPersonneTest(int id) throws Exception {
-        mockMvc.perform(get("/personne/" + id)).andExpect(status().isOk());
+    public void createPersonneTest() throws Exception {
+        // Création du JSON à transmettre
+        String json = "{\"nomPersonne\": \"NOM\",\"prenomPersonne\": \"Prenom\"}";
+
+        // Test de la création de la nouvelle personne
+        mockMvc.perform(MockMvcRequestBuilders.post("/personne")
+                        .contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
     }
 
-//    TODO : Tests des méthodes create/update/delete une fois les modifications du controller effectuées
-//    @Test
-//    public void createPersonneTest(Personne personne) throws Exception {}
-//
-//    public void updatePersonneTest(Personne personne) throws Exception {}
-//
-//    public void deletePersonneTest(Personne personne) throws Exception {}
+    /**
+     * Test de la modification d'une personne
+     *
+     * @throws Exception
+     */
+    @Test
+    public void updatePersonneTest(Personne personne) throws Exception {
+        // Création d'une personne à modifier
+        String jsonCreate = "{\"nomPersonne\":\"NOM\",\"prenomPersonne\":\"Prenom\"}";
+        mockMvc.perform(post("/personne")
+                .contentType(MediaType.APPLICATION_JSON).content(jsonCreate));
+
+        // Création du JSON à transmettre
+        String jsonUpdate = "{\"idPersonne\": 1," +
+                "\"nomPersonne\": \"NOM2\"," +
+                "\"prenomPersonne\": \"Prenom2\"}";
+
+        // Test de modification de la personne
+        mockMvc.perform(MockMvcRequestBuilders.put("/personne/1")
+                        .contentType(MediaType.APPLICATION_JSON).content(jsonUpdate))
+                .andExpect(status().isOk());
+    }
+
+    /**
+     * Test de la suppression d'une personne
+     *
+     * @throws Exception
+     */
+    public void deletePersonneTest() throws Exception {
+        // Création d'une personne à supprimer
+        String json = "{\"nomPersonne\":\"NOM\",\"prenomPersonne\":\"Prenom\"}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/personne")
+                .contentType(MediaType.APPLICATION_JSON).content(json));
+
+        // Vérification du fonctionnement de la suppression
+        mockMvc.perform(delete("/personne/1")).andExpect(status().isOk());
+    }
+
+    /**
+     * Test de l'affichage d'une personne
+     *
+     * @throws Exception
+     */
+    @Test
+    public void getPersonneTest() throws Exception {
+        // Création d'une personne à rechercher
+        String json = "{\"nomPersonne\":\"NOM\",\"prenomPersonne\":\"Prenom\"}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/personne")
+                .contentType(MediaType.APPLICATION_JSON).content(json));
+
+        // Vérification du fonctionnement de la recherche
+        mockMvc.perform(get("/personne/1")).andExpect(status().isOk());
+    }
 
 }
