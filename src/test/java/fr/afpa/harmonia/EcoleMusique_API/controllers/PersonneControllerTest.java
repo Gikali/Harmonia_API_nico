8ff6a6467fdf6b4e0965d819bfac045aed800a4e
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2025-04-07 11:07:31 +0200

    Correction du nom de la classe

--- src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/PersonneControllerTest.java
+++ src/test/java/fr/afpa/harmonia/EcoleMusique_API/controllers/PersonneControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PersonneController.class)
-public class PersonControllerTest {
+public class PersonneControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
