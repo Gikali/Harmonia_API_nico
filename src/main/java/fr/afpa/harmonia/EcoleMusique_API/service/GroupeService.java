b634e3d41f86c4558d19dcfaa1b72bb2462ac8da
CDA-03 <nojnk.nonws @gmail.com>
2025-04-07 13:40:42 +0200

    Code API Groupe ok, tests à faire

---
+++ src/main/java/fr/afpa/harmonia/EcoleMusique_API/service/GroupeService.java
@@ -0,0 +1,34 @@
+package fr.afpa.harmonia.EcoleMusique_API.service;
+
+import fr.afpa.harmonia.EcoleMusique_API.models.Groupe;
+import fr.afpa.harmonia.EcoleMusique_API.repositories.GroupeRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Data
+@Service
+public class GroupeService {
+
+    @Autowired
+    private GroupeRepository groupeRepository;
+
+    public Optional<Groupe> getGroupeById(int id) {
+        return groupeRepository.findById(id);
+    }
+
+    public Iterable<Groupe> getAllGroupes() {
+        return groupeRepository.findAll();
+    }
+
+    public void deleteGroupe(int id) {
+        groupeRepository.deleteById(id);
+    }
+
+    public Groupe saveGroupe(Groupe groupe) {
+        Groupe savedGroupe = groupeRepository.save(groupe);
+        return savedGroupe;
+    }
+}
