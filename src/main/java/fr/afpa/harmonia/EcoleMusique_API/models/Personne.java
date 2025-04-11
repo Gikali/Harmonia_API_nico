e169b0a37823eed5ebc1288c7073e4fccf97f4fc
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2025-04-11 11:55:25 +0200

    Ajout d'une méthode vérifiant la validité des attributs

--- src/main/java/fr/afpa/harmonia/EcoleMusique_API/models/Personne.java
+++ src/main/java/fr/afpa/harmonia/EcoleMusique_API/models/Personne.java
@@ -11,14 +11,35 @@ import lombok.Data;
 @Table(name = "PERSONNE")
 public class Personne {
 
+    /**
+     * Identifiant de la personne
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_personne")
     private int id;
 
+    /**
+     * Nom de la personne
+     */
     @Column(name = "nom_personne", length = 50, nullable = false)
     private String nom;
 
+    /**
+     * Prénom de la personne
+     */
     @Column(name = "prenom_personne", length = 30, nullable = false)
     private String prenom;
+
+
+    /**
+     * Méthode vérifiant la validité des attributs d'une personne.
+     *
+     * @return True si tous les attributs sont valides, False si au moins un attribut est invalide
+     */
+    public boolean validateValues() {
+        return (this.nom != null) && (this.prenom != null)
+                && (!this.nom.isEmpty()) && (!this.prenom.isEmpty())
+                && (this.nom.length() <= 50) && (this.prenom.length() <= 30);
+    }
 }
